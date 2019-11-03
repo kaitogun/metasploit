2cdde239ca5cf5f61e51314043b560f12bf8d33e
kaitogun <37949827+kaitogun@users.noreply.github.com>
2019-11-03 01:45:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!bin/bash
 apt update && apt upgrade -y
-apt install lolcat && apt install toilet && apt install figlet -y
+apt install toilet && apt install figlet -y
 apt install wget && apt install curl && apt install ruby -y
+gem install lolcat -y 
 apt install root-repo && apt install unstable-repo && apt install x11-repo -y
 pkg install metasploit -y
 pkg install postgresql -y
